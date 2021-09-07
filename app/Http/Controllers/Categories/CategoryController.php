431365be87ce6dbc9c25c8410eb03d0451ed4af3
paul-lestyo <lestyo24@gmail.com>
2021-09-07 15:12:55 +0700

    add route and feature CRUD category

--- app/Http/Controllers/Categories/CategoryController.php
+++ app/Http/Controllers/Categories/CategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Categories;
 
-use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
@@ -15,5 +17,57 @@ class CategoryController extends Controller
 			'status' => true,
 			'data' => $categories
 		]);
+	}
+
+	public function show(Category $category) 
+	{
+		return response()->json([
+			'status' => true,
+			'data' => $category
+		],200);
+	}
+
+	public function update(Category $category) 
+	{
+		request()->validate([
+			'name' => 'required',
+		]);
+
+		$category = tap($category)->update([
+			'name' => request('name'),
+			'slug' => Str::slug(request('name'))
+		]);
+
+		
+
+		return response()->json([
+			'status' => true,
+			'data' => $category
+		],200);
+	}
+
+	public function store(Request $request) 
+	{
+		$request->validate([
+			'name' => 'required',
+		]);
+
+		$category = Category::create([
+			'name' => $request->name,
+			'slug' => Str::slug(request('name'))
+		]);
+
+		return response()->json([
+			'status' => true,
+			'data' => $category
+		],200);
+	}
+
+	public function destroy(Category $category) 
+	{
+		$category->delete();
+		return response()->json([
+			'status' => true,
+		],200);
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Blogs\BlogController;
 use App\Http\Controllers\Notes\NoteController;
 use App\Http\Controllers\Notes\SubjectController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Categories\CategoryController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,6 +17,14 @@ Route::prefix('blog')->group(function() {
 	Route::get('{blog:slug}', [BlogController::class, 'show']);
 	Route::put('{blog:slug}', [BlogController::class, 'update']);
 	Route::delete('{blog:slug}', [BlogController::class, 'destroy']);
+});
+
+Route::prefix('category')->group(function() {
+	Route::get('', [CategoryController::class, 'index']);
+	Route::post('create', [CategoryController::class, 'store']);
+	Route::get('{category:slug}', [CategoryController::class, 'show']);
+	Route::put('{category:slug}', [CategoryController::class, 'update']);
+	Route::delete('{category:slug}', [CategoryController::class, 'destroy']);
 });
 
 Route::prefix('notes')->group(function() {
