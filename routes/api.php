a889cfac85538fe7bcbf070e01bb7f44847b6954
paul-lestyo <lestyo24@gmail.com>
2021-09-07 15:14:46 +0700

    delete notes and subjects

--- routes/api.php
+++ routes/api.php
@@ -26,15 +26,3 @@ Route::prefix('category')->group(function() {
 	Route::put('{category:slug}', [CategoryController::class, 'update']);
 	Route::delete('{category:slug}', [CategoryController::class, 'destroy']);
 });
-
-Route::prefix('notes')->group(function() {
-	Route::get('', [NoteController::class, 'index']);
-	Route::post('create-new-note', [NoteController::class, 'store']);
-	Route::get('{note:slug}', [NoteController::class, 'show'])->name('notes.show');
-	Route::patch('{note:slug}/edit', [NoteController::class, 'update']);
-	Route::delete('{note:slug}/delete', [NoteController::class, 'destroy']);
-});
-
-Route::prefix('subjects')->group(function() {
-	Route::get('', [SubjectController::class, 'index']);
-});
